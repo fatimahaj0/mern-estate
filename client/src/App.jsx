c3ba5319e50f198c27318f8bc796504bf50ca90c
Phatima <fatima_haj00@outlook.com>
2024-01-09 18:03:55 +0200

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
-// import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from "./components/PrivateRoute";
 // import CreateListing from './pages/CreateListing';
 // import UpdateListing from './pages/UpdateListing';
 // import Listing from './pages/Listing';
@@ -20,6 +20,9 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
