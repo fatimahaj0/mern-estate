296012e0111cb05dbe890e55826241408a7b62eb
Phatima <fatima_haj00@outlook.com>
2024-01-16 16:11:38 +0200

    image swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 
-// import Listing from './pages/Listing';
-// import Search from './pages/Search';
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -21,6 +21,7 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
