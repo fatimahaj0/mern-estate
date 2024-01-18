33a941cc722389fe511be2b0bd6079bb8b782bbe
Phatima <fatima_haj00@outlook.com>
2024-01-18 17:59:20 +0200

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 const ListingItem = ({ listing }) => {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[283px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[270px] ">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
